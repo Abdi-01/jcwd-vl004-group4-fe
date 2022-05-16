d366e7b6cec455ef72427d012ad1bdb8e1c16887
jeyhunt <social.jeykurniawan@gmail.com>
2022-05-16 22:47:14 +0700

    added transaction history in user profile and register new admin in admin page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import ProductDetail from "../components/ProductDetail";
 import Slider from "../components/Slider";
 import styled from "styled-components";
 import Axios from "axios";
-import { API_URL } from '../constants/API'
+import { API_URL } from "../constants/API";
 import { useSelector, dispatch, useDispatch } from "react-redux";
 
 const ContainerProduct = styled.div`
@@ -15,30 +15,26 @@ const ContainerProduct = styled.div`
   justify-content: space-between;
 `;
 
-
 const Home = () => {
-
-  const userId = useSelector(state => state.authUserLogin.id)
-  const dispatch = useDispatch()
-  const [cartItems, setCartItems] = useState([])
+  const userId = useSelector((state) => state.authUserLogin.id);
+  const dispatch = useDispatch();
+  const [cartItems, setCartItems] = useState([]);
 
   // DATA ASLI
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchCartItems = () => {
-      Axios.get(`${API_URL}/cart/user/${+userId}`)
-        .then(response => {
-          setCartItems(response.data.rows)
-          dispatch({
-            type: "CART_COUNT",
-            payload: response.data.count
-          })
-        })
-    }
-    fetchCartItems()
-  }, [])
-
+      Axios.get(`${API_URL}/cart/user/${+userId}`).then((response) => {
+        setCartItems(response.data.rows);
+        dispatch({
+          type: "CART_COUNT",
+          payload: response.data.count,
+        });
+      });
+    };
+    fetchCartItems();
+  }, []);
 
   useEffect(() => {
     const getProductsData = async () => {
@@ -46,7 +42,7 @@ const Home = () => {
         params: {
           sortField: "createdAt",
           sortDirection: "DESC",
-          limit: 4
+          limit: 4,
         },
       });
       console.log(data.allProducts);
