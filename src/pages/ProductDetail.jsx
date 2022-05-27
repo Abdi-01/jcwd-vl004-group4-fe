1f55a6476dceb5d2c0b532c8cb986f48479b0f4c
Abdi-01 <abdialghi@gmail.com>
2022-05-27 20:04:24 +0700

    fix API_URL

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -256,7 +256,7 @@ const ProductDetail = () => {
           <>
             <ImgContainer>
               {/* bcs productDetail.image is in backend} */}
-              <Image src={API_URL + productDetail.image} />
+              <Image src={API_URL + "/" + productDetail.image} />
             </ImgContainer>
             <InfoContainer>
               <Title>{productDetail.name}</Title>
